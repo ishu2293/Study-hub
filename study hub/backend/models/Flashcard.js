197e98506ae6fc817321e19781ed191edc567e67
ishu2293 <ishwaridaphal4@gmail.com>
2026-03-27 21:26:43 +0530

    Delete study hub directory

--- study hub/backend/models/Flashcard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const flashcardSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  question: String,
-  answer: String
-});
-
-module.exports = mongoose.model("Flashcard", flashcardSchema);
--- study hub/fronted/js/main.js
+++
@@ -1,9 +0,0 @@
-function goToNotes(subject) {
-  localStorage.setItem("selectedSubject", subject);
-  window.location.href = "notes.html";
-}
-
-function logout() {
-  localStorage.removeItem("user");
-  window.location.href = "login.html";
-}
